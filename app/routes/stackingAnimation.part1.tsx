49094b123b74254c7f4acd7c6c100402045ffa37
Nivekithan S <nivekithan363@gmail.com>
2024-07-22 15:32:34 +0530

    Fix bug with overflow on stackingAnimation part 1

--- app/routes/stackingAnimation.part1.tsx
+++ app/routes/stackingAnimation.part1.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { Portal } from "@radix-ui/react-portal";
 
 export function loader() {
   return null;
@@ -26,7 +27,9 @@ function StackingAnimation() {
         <button
           type="button"
           className="px-6 py-4 bg-blue-600 text-white rounded-md text-2xl"
-          onClick={() => setShowMenu(!showMenu)}
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
         >
           Show
         </button>
@@ -37,45 +40,52 @@ function StackingAnimation() {
 
 function MenuContent({ open }: { open: boolean }) {
   return (
-    <motion.div animate={open ? "open" : "closed"} initial={false}>
-      <motion.div
-        variants={{
-          open: {
-            y: "-10%",
-            x: "-50%",
-          },
-          closed: {
-            y: "100%",
-            x: "-50%",
-          },
-        }}
-        transition={{
-          type: "spring",
-          damping: 20,
-          delayChildren: 0.3,
-          staggerChildren: 0.2,
-        }}
-        className="absolute bottom-0 left-1/2"
-      >
-        <ul className="flex flex-col gap-12 p-12 bg-blue-600 rounded-xl text-white text-lg">
-          <Link>Case Studies</Link>
-          <Link>About Us</Link>
-          <Link>Contact Us</Link>
-          <motion.ul
-            className="flex gap-4"
+    <Portal>
+      <div className="h-screen absolute top-0 left-0 w-full overflow-hidden pointer-events-none">
+        <motion.div animate={open ? "open" : "closed"} initial={false}>
+          <motion.div
             variants={{
-              open: { opacity: 1, transition: { type: "spring", damping: 20 } },
-              closed: { opacity: 0 },
+              open: {
+                y: "-10%",
+                x: "-50%",
+              },
+              closed: {
+                y: "100%",
+                x: "-50%",
+              },
             }}
+            transition={{
+              type: "spring",
+              damping: 20,
+              delayChildren: 0.3,
+              staggerChildren: 0.2,
+            }}
+            className="absolute bottom-0 left-1/2"
           >
-            <li>EPYC labs</li>
-            <li>Resources</li>
-            <li>Webflow</li>
-            <li>Privacy Policy</li>
-          </motion.ul>
-        </ul>
-      </motion.div>
-    </motion.div>
+            <ul className="flex flex-col gap-12 p-12 bg-blue-600 rounded-xl text-white text-lg">
+              <Link>Case Studies</Link>
+              <Link>About Us</Link>
+              <Link>Contact Us</Link>
+              <motion.ul
+                className="flex gap-4"
+                variants={{
+                  open: {
+                    opacity: 1,
+                    transition: { type: "spring", damping: 20 },
+                  },
+                  closed: { opacity: 0 },
+                }}
+              >
+                <li>EPYC labs</li>
+                <li>Resources</li>
+                <li>Webflow</li>
+                <li>Privacy Policy</li>
+              </motion.ul>
+            </ul>
+          </motion.div>
+        </motion.div>
+      </div>
+    </Portal>
   );
 }
 
